0970f40ad0679ccd2c64abc25b9ac8cf9d9acca2
PC <rr0263032023@unab.edu.sv>
2025-10-13 14:47:38 -0600

    Problemas con el Android manifest y eliminando una redundancia

--- app/src/main/java/com/example/jardnzen_app/MainActivity.kt
+++ app/src/main/java/com/example/jardnzen_app/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.jardnzen_app
+package com.locochones.jardnzen_app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
