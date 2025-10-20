820329eebc59063b5fd0231893e73485149b561b
Antony <126409554+Antoain@users.noreply.github.com>
2025-10-19 22:05:30 -0600

    Nombre de usuario en el Inicio

--- app/src/main/java/com/example/jardnzen_app/Fragmentos/FragmentInicio.kt
+++ app/src/main/java/com/example/jardnzen_app/Fragmentos/FragmentInicio.kt
@@ -5,20 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.locochones.jardnzen_app.R
+import com.locochones.jardnzen_app.databinding.FragmentInicioBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentInicio.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentInicio : Fragment() {
-    // TODO: Rename and change types of parameters
+    private lateinit var binding: FragmentInicioBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseRef: DatabaseReference
+
+
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,20 +37,36 @@ class FragmentInicio : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        binding = FragmentInicioBinding.inflate(inflater, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val user = firebaseAuth.currentUser
+
+        databaseRef = FirebaseDatabase.getInstance().getReference("Usuarios")
+        if (user != null) {
+            obtenerUsuario(user.uid)
+
+        }else{
+            binding.holaUsuario.text = "Hola ????"
+        }
+        return binding.root
+    }
+
+    private fun obtenerUsuario(uid: String) {
+        databaseRef.child(uid).get().addOnSuccessListener { snapshot ->
+            if (snapshot.exists()) {
+                val nombre = snapshot.child("nombre").value.toString()
+                if (!nombre.isNullOrEmpty()) {
+                    binding.holaUsuario.text = "Hola $nombre"
+                } else {
+                    binding.holaUsuario.text = "Hola ????"
+                }
+
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentInicio.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             FragmentInicio().apply {
