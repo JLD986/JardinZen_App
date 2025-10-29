b9cffa72dfdf0513cb4243449cb946e5fd666ef9
Antony <126409554+Antoain@users.noreply.github.com>
2025-10-29 04:17:25 -0600

    se hizo Serializable

--- app/src/main/java/com/example/jardnzen_app/Modelos/Planta.kt
+++ app/src/main/java/com/example/jardnzen_app/Modelos/Planta.kt
@@ -1,10 +1,13 @@
 package com.example.jardnzen_app.Modelos
 
+import java.io.Serializable
+
 data class Planta(
+   var id: String = "",
    val nombre: String = "",
    val temperatura: String = "",
    val humedad: String = "",
    val luz: String = "",
    val agua: String = "",
    val imagenUrl: String = ""
-)
+) : Serializable
